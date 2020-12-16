9fb3392d5f292e3219f86d345cb00e976928b211
Emre Guclu <emreg@microsoft.com>
2020-12-16 16:08:20 +0300

    day2 - end day

--- Day2_4-PipeLineResults.ps1
+++ Day2_4-PipeLineResults.ps1
@@ -1,1 +1,16 @@
-﻿Get-
+﻿# Format komutları ile pipeline çıktısını şekilendirmek
+## format Table kuıllanımı
+Get-Service | Format-Table
+
+## Format-table ile genişleterek göstermek
+Get-Service | Format-Table -AutoSize -Property *
+gci | Format-Table -AutoSize -Property *
+gci | ft -Property *
+
+# format-list ile tüm propertyleri görmek
+gci | Format-List -Property * -Force
+
+
+# Export komutlar ile pipeline çıktısı farklı dosya türlerine aktarabiliriz.
+gci | Export-Csv -Path c:\Filelist.csv
+gci | Export-Clixml -Path c:\filelist.xml
---
+++ day2_5-LastBlockedSender.ps1
@@ -0,0 +1,16 @@
+﻿$UsersArray = 'emreg@microsoft.com','emreg2@microsoft.com','emreg3@microsoft.com','emreg4@microsoft.com','emreg6@microsoft.com'
+
+$ObjectToWork = [PsCustomObject]@{
+
+    Name = 'Onemli Object'
+    Users = $UsersArray
+
+}
+$LastUseraddedHash = @{
+
+Name = 'LastUserAdded'
+Expression = {($_.Users)[-1]}
+
+}
+$ObjectToWork | Select-Object -Property Name,Users
+$ObjectToWork | Select-Object -Property Name,$LastUseraddedHash
