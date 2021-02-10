60f43249865ca296014e34f71269af697bab57c4
Emre Güçlü <emreguclu@hotmail.com>
2021-02-10 14:37:59 +0300

    added css and html export to reporting certificate ps1

--- Day2_3-Report_Certificate.ps1
+++ Day2_3-Report_Certificate.ps1
@@ -1,24 +1,55 @@
-﻿$PropertyHash=@{
+﻿$HTMLHeader=@"
+<style>
+
+    table {
+		font-size: 12px;
+		border: 0px; 
+		font-family: Arial, Helvetica, sans-serif;
+	} 
+	
+    td {
+		padding: 4px;
+		margin: 0px;
+		border: 0;
+	}
+	
+    th {
+        background: #395870;
+        background: linear-gradient(#49708f, #293f50);
+        color: #fff;
+        font-size: 11px;
+        text-transform: uppercase;
+        padding: 10px 15px;
+        vertical-align: middle;
+	}
+
+    tbody tr:nth-child(even) {
+        background: #f0f0f2;
+    }
+</style>
+
+"@
+
+$DaysToExpire = @{
     
-    Name ='DaysToExpire'
-    Expression = {
-    
-        $DaystoExpire = [Math]::Round(($_.NotAfter - (Get-date)).TotalDays,2)
+    Name='DaysToExpire'
+    Expression={
+        $Days = [math]::Round(($_.NotAfter - (Get-Date)).TotalDays,2)
         
-        # aşağıda -lt 1', prod ortamında -eq 0 olarak değiştirin bir zahmet :)
-        if ($DaystoExpire -lt 1) {
+        if ($Days -le 1) {
             
             'Expired'
         
         } else {
         
-        $DaystoExpire
+        $Days
         
         }
+        
     
     }
 
 }
 
-gci Cert:\LocalMachine\My | Select-Object -Property Thumbprint,Subject,$PropertyHash | Sort-Object -Property DaysToExpire
-
+$Result = gci Cert:\LocalMachine\My | Select-Object -Property Thumbprint,Subject,$DaysToExpire 
+$Result | ConvertTo-Html -Title 'Certificate Report' -head $HTMLHeader  | Out-File -FilePath c:\temp\cert.html
---
+++ Day4_2-ErrorHandling.ps1
@@ -0,0 +1,69 @@
+﻿# Error Handling
+
+# Powershell Doğası
+$ErrorActionPreference
+
+# Continue davranışna örnek
+Get-Service -Name Spooler,olmayanervis
+
+# Continue davranışını terminating'e dönüştirebiliriz.
+Get-Service -Name Spooler,olmayanservis -ErrorAction Stop
+
+## Acemiler için ERror Handling (non-Termating errors)
+Get-Service -Name Spooler,olmayanservis -ErrorAction SilentlyContinue
+if (!$?) {
+
+Write-error "hiçbişi olmadı ise bile birşey olmuştur kesin"
+
+}
+
+
+
+## Profesyoneller için error handling (terminating errors)
+
+### Termiating hatalar örnekler
+<#
+
+1) Runtime hataları 
+    
+    1/0
+
+2) Object methodları
+
+    $Object = [System.Net.WebClient]::new()
+    $Object.DownloadFile('http://server1/emreg.doc')
+
+
+#>
+
+# try catch finally sadewce terminating hataları yakalar, non-terminating hatları terminateinge aşağıdaki dönüştererek try blocku içinde kullanabiliriz.
+try {
+
+Get-Service -Name Spooler,olmayanservis -ErrorAction Stop
+
+} 
+Catch {
+
+throw "hiçbişi olmadı ise bile birşey olmuştur kesin"
+
+}
+
+
+# Daha gelişmiş bir örnek
+
+try
+{
+    1/0
+}
+catch [DivideByZeroException]
+{
+    Write-Host "Divide by zero exception"
+}
+catch [System.Net.WebException],[System.Exception]
+{
+    Write-Host "Other exception"
+}
+finally
+{
+    Write-Host "cleaning up ..."
+}
