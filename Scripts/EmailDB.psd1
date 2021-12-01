8f15c0767c35f06a9508bc9b35a85ba3aee4d2e6
Emre Güçlü <emreg@microsoft.com>
2021-12-01 13:52:27 +0300

    Added new script

---
+++ Scripts/EmailDB.psd1
@@ -0,0 +1,8 @@
+﻿@{
+
+'Infra Ou Guys' = 'infraguys@contoso.org'
+'Middleware Heroes'= 'ortheros@contoso.org'
+'App Management Workers' ='themarabas@contoso.org'
+ 'Default' = 'default@contoso.com'
+
+}
---
+++ Scripts/Send-ExpiredCertsMail.ps1
@@ -0,0 +1,80 @@
+﻿Param(
+
+[string]$EmailDBpath
+
+
+)
+Function New-CertObject {
+
+Param(
+
+[string]$Thumbprint,
+[string]$OU
+
+)
+
+[PscustomObject]@{
+
+    Thumbprint = $Thumbprint
+    Ou = $OU
+
+
+}
+
+}
+
+Function Send-CertEmail {
+
+Param(
+    
+    [string]$Email,
+    [string]$ThumbPrint
+
+)
+
+"Certificate Expired. Thumbprint = $Thumbprint, Email: $Email"
+
+
+}
+Function Get-EmailFromHashtable {
+
+Param(
+
+    $HashTable,
+    $Ou
+
+)
+
+
+
+if ($HashTable.ContainsKey($Ou)) {
+
+$HashTable.$Ou
+
+} else {
+
+$HashTable.Default
+
+}
+
+}
+
+$EmailHashtable = Import-PowerShellDataFile -Path $EmailDBpath
+
+$Cert1 = New-CertObject -Thumbprint 'xxxyyyccctttt234xxxx' -OU 'Infra Ou Guys'
+$Cert2 = New-CertObject -Thumbprint 'ccctttjjjjkkkk234lll' -OU 'Middleware Heroes'
+$cert3 = New-CertObject -Thumbprint 'lllleeewweeestt23llk' -OU 'App Management Workers'
+$cert4 = New-CertObject -Thumbprint 'qqqeweeesss32452vvvv' -OU 'A new Ou that needs to be updated'
+
+$Certs = @($cert1,$Cert2,$cert3,$cert4)
+
+Foreach ($cert in $Certs) {
+
+
+$Email = Get-EmailFromHashtable -HashTable $EmailHashtable -Ou $Cert.Ou
+    
+    
+Send-CertEmail -ThumbPrint $Cert.Thumbprint -Email $Email
+
+    
+}
